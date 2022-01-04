098d47447dfabee9670ebcc324506a595f1f2326
netsage <isaiahtokunbo11@gmail.com>
2022-01-04 13:41:07 +0100

    add jwt gaurd

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtGuard } from './guards/jwt.guard';
+import { JwtStrategy } from './guards/jwt.strategy';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from './entities/user.entity';
 
 @Module({
-  providers: [AuthService],
+  imports:[
+    JwtModule.registerAsync({
+      useFactory: () => ({
+       secret: process.env.JWT_SECRET,
+       signOptions: { expiresIn:parseInt(<string>process.env.JWT_EXPIRES)}
+      }), 
+     }),  
+    TypeOrmModule.forFeature([UserEntity])],
+  providers: [AuthService,JwtGuard,JwtStrategy],
   controllers: [AuthController],
 })
 export class AuthModule {}
