68eb2ced2ca55aa503525342668d1f5c3a03800b
netsage <isaiahtokunbo11@gmail.com>
2022-01-04 14:38:58 +0100

    validate authdto

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,10 +1,11 @@
-import { IsEmail, IsNotEmpty } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsNumber } from 'class-validator';
 
 export class AuthDTO {
   id?: number;
   @IsNotEmpty()
   fullName?: string;
   @IsNotEmpty()
+  @IsNumber()
   phone?: number;
   @IsEmail()
   email?: string;
